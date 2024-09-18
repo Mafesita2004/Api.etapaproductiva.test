ba04f79039fddb5d02fe582d4e3a56d090a7e7aa
Diemaf2004 <mariafernandacalvachemunoz21@gmail.com>
2024-09-18 10:34:41 -0500

    llaves

--- app/Models/User_register.php
+++ app/Models/User_register.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class User_register extends Model
 {
     use HasFactory;
+    public function Role(){
+        return $this->belongsTo('App\Models\Role');
+    }
+    public function Contract(){
+        return $this->belongsTo('App\Models\Contract');
+    }
+    public function Followup(){
+        return $this->belongsTo('App\Models\Followup');
+    }
+     public function Company(){
+            return $this->belongsTo('App\Models\Company');
+        
+    }
+    public function Academic_level(){
+        return $this->belongsTo('App\Models\Academic_level');
+    
+}
+public function Knowledge_network(){
+    return $this->belongsTo('App\Models\Knowledge_network');
+
+}
+
+public function Contract_type(){
+    return $this->belongsTo('App\Models\Contract_type');
+
+}
+
 }
