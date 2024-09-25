7310c87281be1c9c4ea1e63d2dc1789a51454aaf
diemaf2004 <mariafernandacalvachemunoz21@gmail.com>
2024-09-25 11:43:15 -0500

    PARTE MAFE(TABLAS,MODELOS,CONTROLADORES)

--- app/Http/Controllers/AcademicLevelController.php
+++ app/Http/Controllers/AcademicLevelController.php
@@ -7,59 +7,43 @@ use Illuminate\Http\Request;
 
 class AcademicLevelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $academic_level = Academic_level::included()->filter()->sort()->get();
+        return response()->json($academic_level);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255',
+        ]);
+
+        $academic_level= Academic_level::create($request->all());
+
+        return response()->json($academic_level, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Academic_level $academic_level)
+    public function show($id)
     {
-        //
+        $academic_level= Academic_level::included()->findOrFail($id);
+        return response()->json($academic_level);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Academic_level $academic_level)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Academic_level $academic_level)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255',
+        ]);
+
+        $academic_level->update($request->all());
+
+        return response()->json($academic_level);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Academic_level $academic_level)
     {
-        //
+        $academic_level->delete();
+        return response()->json(null, 204);
     }
 }
