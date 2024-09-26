e67810af2ed8101862018cd1b7a9338ac812d876
unknown <astridcantero2005@gmail.com>
2024-09-25 20:00:58 -0500

    subir apiroutes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DiaryController;
+use App\Http\Controllers\ProgramController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    Route::get('programs', [ProgramController::class,'index'])->name('api.programs.index');
+    Route::post('programs', [ProgramController::class,'store'])->name('api.programs.store');
+   Route::get('programs/{program}', [ProgramController::class,'show'])->name('api.programs.show');
+  Route::put('programs/{program}', [ProgramController::class,'update'])->name('api.programs.update'); 
+   Route::delete('programs/{program}', [ProgramController::class,'destroy'])->name('api.programs.delete');
+
+
+   Route::get('diaries', [DiaryController::class,'index'])->name('api.diaries.index');
+   Route::post('diaries', [DiaryController::class,'store'])->name('api.diaries.store');
+  Route::get('diaries/{diary}', [DiaryController::class,'show'])->name('api.diaries.show');
+ Route::put('diaries/{diary}', [DiaryController::class,'update'])->name('api.diaries.update'); 
+  Route::delete('diaries/{diary}', [DiaryController::class,'destroy'])->name('api.diaries.delete');
+
+
+  Route::get('companies', [CompanyController::class,'index'])->name('api.companies.index');
+  Route::post('companies', [CompanyController::class,'store'])->name('api.companies.store');
+ Route::get('companies/{company}', [CompanyController::class,'show'])->name('api.companies.show');
+Route::put('companies/{company}', [CompanyController::class,'update'])->name('api.companies.update'); 
+ Route::delete('companies/{company}', [CompanyController::class,'destroy'])->name('api.companies.delete');
